e10136cfdfb5b880acd82fd4223c1ff4f2e9ab2f
Andrew Dodd <atd7@cornell.edu>
2011-08-03 16:57:56 -0400

    Add undervolting interface via sysfs
    Non-OC variant of netarchy's voltage control patches
    
    Signed-off-by: Andrew Dodd <atd7@cornell.edu>

--- arch/arm/mach-s5pv210/cpu-freq_c110b.c
+++ arch/arm/mach-s5pv210/cpu-freq_c110b.c
@@ -50,6 +50,9 @@ static unsigned int mpll_freq; /* in MHz */
 static unsigned int apll_freq_max; /* in MHz */
 static DEFINE_MUTEX(set_freq_lock);
 
+/* UV */
+extern int exp_UV_mV[5]; 
+
 /* frequency */
 static struct cpufreq_frequency_table freq_table[] = {
 	{L0, 1200*1000},
@@ -506,7 +509,7 @@ static int s5pv210_cpufreq_target(struct cpufreq_policy *policy,
 	if (s3c_freqs.freqs.new == s3c_freqs.freqs.old && !first_run)
 		goto out;
 
-	arm_volt = dvs_conf[index].arm_volt;
+	arm_volt = exp_UV_mV[index]; //dvs_conf[index].arm_volt;
 	int_volt = dvs_conf[index].int_volt;
 
 	/* New clock information update */
@@ -689,7 +692,8 @@ static int s5pv210_cpufreq_target(struct cpufreq_policy *policy,
 	memcpy(&s3c_freqs.old, &s3c_freqs.new, sizeof(struct s3c_freq));
 	cpufreq_debug_printk(CPUFREQ_DEBUG_DRIVER, KERN_INFO,
 			"cpufreq: Performance changed[L%d]\n", index);
-	previous_arm_volt = dvs_conf[index].arm_volt;
+//	previous_arm_volt = dvs_conf[index].arm_volt;
+	previous_arm_volt = exp_UV_mV[index];
 
 	if (first_run)
 		first_run = false;
@@ -732,7 +736,7 @@ static int s5pv210_cpufreq_resume(struct cpufreq_policy *policy)
 
 	memcpy(&s3c_freqs.old, &clk_info[level],
 			sizeof(struct s3c_freq));
-	previous_arm_volt = dvs_conf[level].arm_volt;
+	previous_arm_volt = exp_UV_mV[level]; //dvs_conf[level].arm_volt;
 
 	return ret;
 }
@@ -800,7 +804,7 @@ static int __init s5pv210_cpufreq_driver_init(struct cpufreq_policy *policy)
 
 	memcpy(&s3c_freqs.old, &clk_info[level],
 			sizeof(struct s3c_freq));
-	previous_arm_volt = dvs_conf[level].arm_volt;
+	previous_arm_volt = exp_UV_mV[level]; //dvs_conf[level].arm_volt;
 
 #ifdef CONFIG_DVFS_LIMIT
 	for(i = 0; i < DVFS_LOCK_TOKEN_NUM; i++)
